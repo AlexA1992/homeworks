d0d0918469406acea5800c2c6daa8bf9362d1ddf
Amundsen <gamburk@yandex.ru>
2022-03-15 20:34:09 +0300

    new likes

--- src/main/kotlin/ru/netology/Likes.kt
+++ src/main/kotlin/ru/netology/Likes.kt
@@ -1,17 +1,22 @@
 package ru.netology
 
 fun main(){
-    val likes: Int = 127
-    if ((likes % 10 == 1 || likes == 1) && likes != 11) {
+    val likes: Int = 123
+    if ((likes % 10 == 1 || likes == 1) && (likes != 11 && likes % 100 != 11)) {
         println("Вы получили: $likes" + " лайк")
+    }
+    else if (likes == 11 && likes % 100 == 11)  {
+        println("Вы получили: $likes" + " лайков")
     }
     else if (likes >= 2 && likes <= 4){
         println("Вы получили: $likes" + " лайка")
     }
-    else if (likes >= 5 && likes <= 20) {
-        println("Вы получили: $likes" + " лайков")}
+    else if (likes >= 5 && likes % 10 != 2 && likes % 10 != 3 && likes % 10 != 4) {
+        println("Вы получили: $likes" + " лайков")
+    }
     else if (likes % 10 >= 2 && likes % 10 <= 4) {
         println("Вы получили: $likes" + " лайка")
-    } else {println("Вы получили: $likes" + " лайков")}
+    }
+    else {println("Вы получили: $likes" + " лайков")}
 
 }
